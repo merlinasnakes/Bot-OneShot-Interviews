dcfca1dd44fa7c8401b8ef957c33ffb520102d24
merlinasnakes <caecilia.ae@gmail.com>
2023-05-14 18:29:08 -0300

    intento de función get_membersChannel()

--- src/index.py
+++ src/index.py
@@ -28,12 +28,22 @@ async def on_message(message):
     if message.content.find("hello") != -1:
         await message.channel.send(pickPregunta())    
 
-
-
 @bot.event
 async def ping(ctx):
-    channel = bot.get_channel(978319098834468864)
-    await channel.send('Pong!')
+    channel_target = bot.get_guild(978319098834468864)
+    await channel_target.send('Pong!')
     
+@bot.event    
+async def get_membersChannel():
+    channel_target = bot.get_guild(978319098834468864)
+    #members = channel_target.get_all_members() 
+    members = channel_target.members
+    membersID = [] 
+    for member in members:
+        membersID.append(member.id)
+    print(membersID)  
+            
+get_membersChannel()
     
 bot.run(os.getenv('TOKEN_BOT'))
+
