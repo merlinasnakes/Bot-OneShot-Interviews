4c9210472e133603daab9e4ad40151a2fe083447
merlinasnakes <caecilia.ae@gmail.com>
2023-05-19 20:52:37 -0300

    Se completó la función getMembers con exito

--- src/index.py
+++ src/index.py
@@ -13,10 +13,6 @@ bot = discord.Client(intents=intents)
 #bot = commands.Bot(command_prefix='>', description="This is a survey bot")
 
 
-@bot.event
-async def ping(ctx):
-    await ctx.sent('pong ')
-
 #Events   
 @bot.event 
 async def on_ready():
@@ -26,23 +22,23 @@ async def on_ready():
 async def on_message(message):
     id = bot.get_guild(978319098834468864)
     if message.content.find("hello") != -1:
-        await message.channel.send(pickPregunta())    
-
-@bot.event
-async def ping(ctx):
-    channel_target = bot.get_guild(978319098834468864)
-    await channel_target.send('Pong!')
+        await message.channel.send(pickPregunta())
     
-@bot.event    
-async def get_membersChannel(message):
     if message.content.find("miembros") != -1:
-        channel_target = bot.get_channel(978319098834468864)
+        guild = message.channel.guild
+        print(guild)
+        lista_miembros = []
+        for member in guild.members:
+            lista_miembros.append(member)                
+        await message.channel.send(f"{lista_miembros[1].mention} - {pickPregunta()}")    
+        
+"""     channel_target = bot.get_channel(978319098834468864)
         members = bot.get_all_members() 
         #members = channel_target.members
         membersID = [] 
         for member in members:
             membersID.append(member.id)
-        print(membersID)  
+        print(membersID)   """
             
     
 bot.run(os.getenv('TOKEN_BOT'))
