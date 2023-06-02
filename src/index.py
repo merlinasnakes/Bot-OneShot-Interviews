552ed8db087fafd16f6b8c24d11afbcb60c55481
merlinasnakes <caecilia.ae@gmail.com>
2023-06-02 19:28:26 -0300

    intento de feature

--- src/index.py
+++ src/index.py
@@ -1,6 +1,7 @@
 import discord
 import os
 from discord.ext import commands
+from discord import app_commands
 from dotenv import load_dotenv
 from funcionesPreguntas import pickPregunta
 
@@ -12,11 +13,15 @@ bot = discord.Client(intents=intents)
 
 #bot = commands.Bot(command_prefix='>', description="This is a survey bot")
 
-
 #Events   
 @bot.event 
 async def on_ready():
     print('Bender is ready')
+    try:
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} command(s)")
+    except Exception as e:
+        print(e)    
 
 @bot.event
 async def on_message(message):
@@ -30,7 +35,14 @@ async def on_message(message):
         lista_miembros = []
         for member in guild.members:
             lista_miembros.append(member)                
-        await message.channel.send(f"{lista_miembros[1].mention} - {pickPregunta()}")    
+        await message.channel.send(f"{lista_miembros[1].mention} - {pickPregunta()}")   
+
+@bot.tree.command(name="hello")
+async def hello(interaction: discord.Interaction):
+    await interaction.response.message.send(f"Hey {interaction.user.mention}! This is a slash command!")
+    
+    
+         
         
 """     channel_target = bot.get_channel(978319098834468864)
         members = bot.get_all_members() 
